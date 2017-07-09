3b72d31e7f8301b0bd0bc995cfbe37d570070ead
zhufengnodejs <zhufengnodejs@126.com>
2017-07-09 15:58:28 +0800

    3.完成任务

--- tasks/main.js
+++ tasks/main.js
@@ -1,9 +1,13 @@
 let read = require('./read');
 let write = require('./write');
 let async = require('async');
+let {Movie} = require('../model');
 let url = 'http://top.baidu.com/buzz?b=26';
 async.waterfall([
   function(callback){
+    Movie.remove({},callback);
+  },
+  function(data,callback){
      read(url,callback);
   },
   function(movies,callback){
