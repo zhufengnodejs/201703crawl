22e07e56324c026c674bf0fd81721b04d8f4c515
zhufengnodejs <zhufengnodejs@126.com>
2017-07-09 15:27:55 +0800

    1.获取数据

---
+++ tasks/read.js
@@ -0,0 +1,32 @@
+let request = require('request');
+let iconv = require('iconv-lite');
+let cheerio = require('cheerio');
+// model=模型 module=模块
+module.exports = function (url, callback) {
+  request({url, encoding: null}, function (err, res, body) {
+    //如果没有错误并且响应的状态码为200的时候
+    if (!err && res.statusCode == 200) {
+      //把一个GBK类型的Buffer转成一个utf8的字符串
+      body = iconv.decode(body, 'gbk');
+      //把字符串转成jQuery对象
+      let $ = cheerio.load(body);
+      //可以迭代所有的A标签 拥有keyword类下面的list-title类的A标签
+      //得到的是一个集合，可以通过each进行循环迭代 index索引 item每个元素
+      let movies = [];
+      $('.keyword a.list-title').each(function(index,item){
+         let $this = $(item);
+         let movie = {
+           name:$this.text(),//a标签里的文本就是电影的名字
+           url:$this.attr('href')//a标签的href属性就是超链接
+         }
+         movies.push(movie);
+      });
+      callback(err,movies);
+    }
+  });
+}
+
+let url = 'http://top.baidu.com/buzz?b=26';
+module.exports(url, function (err, movies) {
+  console.log(movies);
+});
